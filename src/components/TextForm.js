b8c28110528f6140849aea961042621ad05dc3ec
Nur Ahmed <84973115+nurahmed123@users.noreply.github.com>
2022-04-20 20:28:34 +0600

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -58,6 +58,17 @@ export default function TextForm(props) {
         window.URL.revokeObjectURL(url);
         props.showAlert("Download started", "success")
     }
+    
+    const printText = () => {
+        childWindow = window.open('','childWindow','location=yes, menubar=yes, toolbar=yes');
+        childWindow.document.open();
+        childWindow.document.write('<html><head></head><body>');
+        childWindow.document.write(document.getElementById('textBox').value.replace(/\n/gi,'<br>'));
+        childWindow.document.write('</body></html>');
+        childWindow.print();
+        childWindow.document.close();
+        childWindow.close();
+    }
     return (
         <>
             <div className="container">
@@ -75,6 +86,7 @@ export default function TextForm(props) {
                     <button disabled={text.length===0} type="button" onClick={handleUpClick} className={`btn btn-outline-${props.mode === "dark" ? "light" : "dark"} mx-1 my-1`}>{btnText} case</button>
                     <button disabled={text.length===0} type="button" onClick={handleClearClick} className={`btn btn-outline-${props.mode === "dark" ? "light" : "dark"} mx-1 my-1`}>Clear area</button>
                     <button disabled={text.length===0} type="button" onClick={downloadFile} className={`btn btn-outline-${props.mode === "dark" ? "light" : "dark"} mx-1 my-1`}>Download document</button>
+                    <button disabled={text.length===0} type="button" onClick={printText} className={`btn btn-outline-${props.mode === "dark" ? "light" : "dark"} mx-1 my-1`}>Print</button>
                     <button disabled={text.length===0} type="button" onClick={handleCopy} className={`btn btn-outline-${props.mode === "dark" ? "light" : "dark"} mx-1 my-1`}>Copy text</button>
                     {/* <button onClick={readText} className="fas fa-volume-down btn btn-outline-dark mx-1 fs-4 px-3"></button> */}
                     <button disabled={text.length===0} onClick={readText} className={`fas fa-volume-down btn btn-outline-${props.mode === "dark" ? "light" : "dark"} mx-1 fs-4 px-3`}></button>
